c550374f41636fb4d2ffb65554d05fd315c6fecf
TopIdiot <1161702621@qq.com>
2017-02-27 16:49:45 +0800

    delete type file

--- C/dmio_type.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "dmio_type.h"
-int get_proc_type(int rank)
-{
-    if(rank == 0) return proc_master;
-    else if(rank <= client_num) return proc_client;
-    return proc_server;
-}
